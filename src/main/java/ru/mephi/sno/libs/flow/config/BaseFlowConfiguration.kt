5bc5042ee181ccb8eb2942615e51b452fa7a1470
Ivan <idfedorov09@gmail.com>
2024-05-25 22:11:31 +0300

    new getClassName mechanics

--- src/main/java/ru/mephi/sno/libs/flow/config/BaseFlowConfiguration.kt
+++ src/main/java/ru/mephi/sno/libs/flow/config/BaseFlowConfiguration.kt
@@ -2,23 +2,23 @@ package ru.mephi.sno.libs.flow.config
 
 import ru.mephi.sno.libs.flow.belly.FlowBuilder
 import ru.mephi.sno.libs.flow.registry.FlowRegistry
-import java.util.*
+import ru.mephi.sno.libs.flow.util.Helps
+import kotlin.reflect.KClass
 
 /**
  * Один из базовых способов создания flow.
  * Регистрирует ваш flow в FlowRegistry.
  * Может быть полезно в связке со Spring Framework и инъекцией фетчеров.
  */
-abstract class BaseFlowConfiguration {
-
-    private val guid = UUID.randomUUID()
-
-    open fun getName() = guid.toString()
+abstract class BaseFlowConfiguration(
+    private val origin: KClass<out BaseFlowConfiguration>
+) {
+    open fun getName(): String = Helps.classStringForm(origin.java)
 
     open fun flowBuilder(): FlowBuilder {
         val flowBuilder = FlowBuilder()
         flowBuilder.buildFlow()
-        FlowRegistry.getInstance().register(getName(), guid, flowBuilder)
+        FlowRegistry.getInstance().register(getName(), origin.java, flowBuilder)
         return flowBuilder
     }
 
---
+++ src/main/java/ru/mephi/sno/libs/flow/util/Helps.java
@@ -0,0 +1,8 @@
+package ru.mephi.sno.libs.flow.util;
+
+public class Helps {
+
+	public static String classStringForm(Class<?> clazz) {
+		return clazz.getName();
+	}
+}
